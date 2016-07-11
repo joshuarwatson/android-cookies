cc814af92dc2b8be8e862b954eb10665fbf9cfa9
DSU App Academy <dsuappacademy@DSUs-MacBook-Pro-2.local>
2016-07-11 16:00:06 -0400

    Initial commit

---
+++ app/src/main/java/edu/desu/newproject/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.desu.newproject;
+
+import android.graphics.Color;
+import android.icu.text.RelativeDateTimeFormatter;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+}
